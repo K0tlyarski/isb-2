54469656ad8b548dbb4d9b9f26b5873549b0f3d8
K0tlyarski <djgame2.9.6@gmail.com>
2023-04-03 18:07:23 +0400

    Initial Commit

---
+++ lab2ISB/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab2ISB
+//
+//  Created by Вадим Котлярский on 03.04.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
