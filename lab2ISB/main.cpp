de2a81d5b83fa1177d3b633219c64a61d3034b55
K0tlyarski <djgame2.9.6@gmail.com>
2023-04-20 12:56:50 +0400

    Implemented the function of displaying a vector on the screen, as well as the function of obtaining a sequence of 128 bits in length, in which this sequence is output to a file

--- lab2ISB/main.cpp
+++ lab2ISB/main.cpp
@@ -6,6 +6,37 @@
 //
 
 #include <iostream>
+#include <random>
+#include <vector>
+#include <fstream>
+
+void show_vector( std::vector<int>&a)
+{
+    for (std::vector<int>::iterator it = a.begin() ; it!=a.end() ; ++it)
+        std::cout<<*it;
+}
+std::vector<int>GetSubsequence (){
+    std::vector<int> res;
+    //Источник
+    std::random_device rd;
+      //Генератор
+    std::default_random_engine generator(rd());
+      // Распределение
+    std::uniform_int_distribution<int> distribution(0,1);
+    for (int i = 0; i < 128; ++i) {
+        res.push_back(distribution(generator));
+    }
+    show_vector(res);
+    std::ofstream file;
+    file.open("/Users/vadimkotlarskij/Desktop/c++/sem4/testRandom/testRandom/binarysequence.txt");
+    if (file.is_open()) {
+        for (auto i : res) {
+            file << i;
+        }
+        }
+        file.close();
+    return res;
+}
 
 int main(int argc, const char * argv[]) {
     
